92ff4ab136904d233c0c62bd274e2a7fe695a67c
Victor Peres <victor.peres93@gmail.com>
2017-07-06 14:43:19 -0300

    normal lighting in voronoi map

--- map-from-voronoi.py
+++ map-from-voronoi.py
@@ -1,7 +1,6 @@
 import numpy as np
 import scipy.spatial as sp
 import matplotlib.pyplot as plt
-from nose.config import all_config_files
 
 from noise_heightmap import NoiseHeightmap
 
@@ -29,40 +28,25 @@ for i in xrange(voronoi.vertices.shape[0]):
         j = j + 1
 new_vertices = np.delete(voronoi.vertices, to_remove, 0)
 
-# print 'removed', to_remove
-# print voronoi.vertices
-# print removed
 # sp.voronoi_plot_2d(voronoi)
-# print "n of vertices: ", (voronoi.points.shape[0] + new_vertices.shape[0])
 n_vertices = voronoi.points.shape[0] + new_vertices.shape[0]
-
-# print voronoi.points
-# print voronoi.vertices
-
 # plt.show()
 all_vertices = np.append(new_vertices, points, 0)
 
 
-from math import pi, sin, cos
 from panda3d.core import *
 from direct.showbase.ShowBase import ShowBase
 import math
 
-import numpy
 from direct.task import Task
-
-DENSITY = 1.0
-
-
-
 
 class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
         
         # create GeomVertexFormat
-        # format = GeomVertexFormat.getV3n3c4()
-        format = GeomVertexFormat.getV3c4()
+        format = GeomVertexFormat.getV3n3c4()
+        # format = GeomVertexFormat.getV3c4()
         # create GeomVertexData
         vdata = GeomVertexData('name', format, Geom.UHStatic)
         # set the number of rows, which in this case is the number of vertices
@@ -73,50 +57,16 @@ class MyApp(ShowBase):
         color = GeomVertexWriter(vdata, 'color')
         
         # fill vertices
-        # height = NoiseHeightmap.noise_heightmap_from_points(n_vertices)
         height_vertices = NoiseHeightmap.height_from_coords(all_vertices)
         
         for i in xrange(all_vertices.shape[0]):
             vertex.addData3f(all_vertices[i][0], all_vertices[i][1], height_vertices[i])
             color.addData4f(.5, 1, 1, 1)
-        # for i in xrange(voronoi.points.shape[0]):
-        #     vertex.addData3f(voronoi.points[i][0], voronoi.points[i][1], height[voronoi.points[i][0], voronoi.points[i][1]])
-        #     color.addData4f(.5, 1, 1, 1)
-
-        # for i in xrange(all_vertices.shape[0]):
-        #     if (all_vertices[i][0] == MAX - 1 or all_vertices[i][1] == MAX - 1 or all_vertices[i][0] == 0 or all_vertices[i][1] == 0):
-        #         normal.addData3f(1, 1, 1)
-        #         continue
-        #     hL = int(height[new_vertices[i][0] - 1, new_vertices[i][1]])
-        #     hR = int(height[new_vertices[i][0] + 1, new_vertices[i][1]])
-        #     hD = int(height[new_vertices[i][0], new_vertices[i][1] + 1])
-        #     hU = int(height[new_vertices[i][0], new_vertices[i][1] - 1])
-        #     n = numpy.zeros(3, numpy.int32)
-        #     n[0] = hL - hR
-        #     n[1] = hU - hD
-        #     n[2] = 1
-        #     norm = math.sqrt(n[0] ** 2 + n[1] ** 2 + n[2] ** 2)
-        #     n = [n[0] / norm, n[1] / norm, n[2] / norm]
-        #     normal.addData3f(n[0], n[1], n[2])
-
-        # for i in xrange(voronoi.points.shape[0]):
-        #     if (voronoi.points[i][0] == MAX - 1 or voronoi.points[i][1] == MAX - 1 or voronoi.points[i][0] == 0 or voronoi.points[i][1] == 0):
-        #         normal.addData3f(1, 1, 1)
-        #         continue
-        #     hL = int(height[voronoi.points[i][0] - 1, voronoi.points[i][1]])
-        #     hR = int(height[voronoi.points[i][0] + 1, voronoi.points[i][1]])
-        #     hD = int(height[voronoi.points[i][0], voronoi.points[i][1] + 1])
-        #     hU = int(height[voronoi.points[i][0], voronoi.points[i][1] - 1])
-        #     n = numpy.zeros(3, numpy.int32)
-        #     n[0] = hL - hR
-        #     n[1] = hU - hD
-        #     n[2] = 1
-        #     norm = math.sqrt(n[0] ** 2 + n[1] ** 2 + n[2] ** 2)
-        #     n = [n[0] / norm, n[1] / norm, n[2] / norm]
-        #     normal.addData3f(n[0], n[1], n[2])
         
         # fill geoprimitive which are triangles
         triangles = GeomTriangles(Geom.UH_static)
+        
+        normals = np.zeros((len(all_vertices), 3))
         
         for p_index in xrange(len(voronoi.point_region)):
             p_region_index = voronoi.point_region[p_index]
@@ -125,9 +75,35 @@ class MyApp(ShowBase):
                 vert_index1 = region[i]
                 vert_index2 = region[(i+1) % len(region)]
                 if vert_index1 > -1 and vert_index2 > -1 and not removed[vert_index1] and not removed[vert_index2]:
+                    v1 = [all_vertices[new_indices[vert_index1]][0],
+                          all_vertices[new_indices[vert_index1]][1],
+                          height_vertices[new_indices[vert_index1]]]
+                    v2 = [all_vertices[new_indices[vert_index2]][0],
+                          all_vertices[new_indices[vert_index2]][1],
+                          height_vertices[new_indices[vert_index2]]]
+                    p = [all_vertices[len(new_vertices) + p_index][0],
+                         all_vertices[len(new_vertices) + p_index][1],
+                         height_vertices[len(new_vertices) + p_index]]
+                    
+                    cross_product = np.cross(np.subtract(v1,v2), np.subtract(v2,p))
+                    if (cross_product[2] < 0): #inverte o sinal
+                        cross_product *= -1
+                    normals[new_indices[vert_index1]] += cross_product
+                    normals[new_indices[vert_index2]] += cross_product
+                    normals[len(new_vertices) + p_index] += cross_product
                     triangles.addVertices(new_indices[vert_index1],
                                           new_indices[vert_index2],
                                           len(new_vertices) + p_index)
+                if (len(region) == 2):  # para nao repetir a combinacao quando forem apenas 2 vertices na regiao
+                    break
+
+        for i in xrange(all_vertices.shape[0]):
+            norm = math.sqrt(normals[i][0] ** 2 + normals[i][1] ** 2 + normals[i][2] ** 2)
+            if norm == 0: # no caso de ponto sem triângulos
+                n = [0,0,0]
+            else:
+                n = [normals[i][0] / norm, normals[i][1] / norm, normals[i][2] / norm]
+            normal.addData3f(n[0], n[1], n[2])
         
         # specifics of panda3d, geom and geomnode and scene graph
         terrainGeom = Geom(vdata)
@@ -162,6 +138,7 @@ class MyApp(ShowBase):
         #
         # Enable the shader generator for the receiving nodes
         self.render.setShaderAuto()
+        # self.render.setRenderModeWireframe()
         #
         # directionalLightNP.setDepthOffset(1)
         
